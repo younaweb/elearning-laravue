d07e89b3bc28bde888f8dc17fdf403f0d0fc91c7
younaweb <younaweb@gmail.com>
2021-09-08 21:10:16 +0100

    create model migration factory seeder for course & video

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,13 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    
+
+    public function courses()
+    {
+        return $this->hasMany(Course::class);
+    }
+
+   
 }
