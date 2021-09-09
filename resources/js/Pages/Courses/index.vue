63c9de1567d5cfe265db745e115b158ed5aa581d
younaweb <younaweb@gmail.com>
2021-09-09 13:18:01 +0100

    fix merge problem

---
+++ resources/js/Pages/Courses/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      branch
+  </div>
+</template>
+
+<script>
+import AppLayouts from './../../Layouts/AppLayout'
+export default {
+components:{
+    AppLayouts
+},
+mounted(){
+    console.log(courses);
+},
+props:{
+    courses
+}
+
+}
+</script>
+
+<style>
+
+</style>
